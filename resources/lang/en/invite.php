c189d0bb583c990f5a5d72b21dc1b7b851df9677
Lester Hurtado <lester@hurtado.ph>
2018-11-06 13:02:58 +0800

    complete invite language

--- resources/lang/en/invite.php
+++ resources/lang/en/invite.php
@@ -2,11 +2,17 @@
 
 return [
 	'input' => [
+		'code' => 'Input code for downline - operator, staff, worker, subscriber:',
+		'mobile' => 'Input mobile number:',	
 		'telegram' => 'Yes, send to Telegram.',
 		'facebook' => 'Yes, send to Facebook.',
 		'no' => 'No, please repeat.',
-		'verify' => 'Verify: code=:code, mobile=:mobile [Yes/No]?',
+		'verify' => 'Verify: role=:code, mobile=:mobile [Yes/No]?',
 	],
+	'introduction' => "You may invite downlines here. Enter /stop anywhere to break the loop.",
 	'notification' => "Hi. :name invites you to join 'One on Juan with Manong Johnny' - A Virtual Talk Show. Click :url to enlist.",
 	'accepted' => "Hi. :name accepted your invitation. - serbis.io",
+	'processing' => "Please stand by - processing...",
+	'sent' => "Invitation has been sent.",
+	'fail' => "Something is wrong. Please notify the system administrator.",
 ];
