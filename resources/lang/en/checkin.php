58511c180058a927eeb52ceeb737e31d3dc881f6
Apple Hurtado <aph@Apples-MacBook-Pro.local>
2018-11-15 11:42:12 +0800

    add checkin language file

---
+++ resources/lang/en/checkin.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+	'introduction' => ":name, you are able to input your location here. Enter /stop anywhere to break the loop.",
+	'processing' => "Please stand by - processing...",
+	'processed' => "Finished processing.",
+	'finished' => "Done.",
+];
